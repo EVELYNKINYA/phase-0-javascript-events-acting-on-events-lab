af6aae9f49d1b4658ef1f5cbafc5f19ffa55ce47
EVELYNKINYA <evelynkinya156@gmail.com>
2024-01-03 20:10:36 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+let dodger;
+
+function moveDodgerLeft() {
+  let currentPosition = dodger.style.left || '0px';
+  let currentPositionInt = parseInt(currentPosition);
+
+  
+  dodger.style.left = `${currentPositionInt - 5}px`;
+}
+
+function moveDodgerRight() {
+  let currentPosition = dodger.style.left || '0px';
+  let currentPositionInt = parseInt(currentPosition);
+
+  
+  dodger.style.left = `${currentPositionInt + 5}px`;
+}
